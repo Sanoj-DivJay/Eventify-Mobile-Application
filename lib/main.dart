db0f523f9802da1bd0333ef59aac8dadde037aba
Janani Waragoda <96419667+JananiWaragoda@users.noreply.github.com>
2023-09-03 22:33:38 +0530

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/page-1/create-event.dart';
+import 'package:myapp/page-1/create_event.dart';
 import 'package:myapp/page-1/dashboard.dart';
-import 'package:myapp/page-1/event-register.dart';
-import 'package:myapp/page-1/get-started.dart';
+import 'package:myapp/page-1/event_register.dart';
+import 'package:myapp/page-1/get_started.dart';
 import 'package:myapp/page-1/home.dart';
 import 'package:myapp/page-1/login.dart';
 import 'package:myapp/page-1/register.dart';
@@ -14,13 +14,13 @@ import 'package:myapp/utils.dart';
 //import 'package:myapp/page-1/home.dart';
 import 'package:myapp/page-1/login.dart';
 // import 'package:myapp/page-1/get-started.dart';
-import 'package:myapp/page-1/loading-screen.dart';
+import 'package:myapp/page-1/loading_screen.dart';
 // import 'package:myapp/page-1/home.dart';
 // import 'package:myapp/page-1/calendar.dart';
 // import 'package:myapp/page-1/dashboard.dart';
 // import 'package:myapp/page-1/map.dart';
-// import 'package:myapp/page-1/event-details.dart';
-//import 'package:myapp/page-1/event-register.dart';
+// import 'package:myapp/page-1/event_details.dart';
+//import 'package:myapp/page-1/event_register.dart';
 // import 'package:myapp/page-1/create-event.dart';
 // import 'package:myapp/page-1/saved-upcoming-events.dart';
 // import 'package:myapp/page-1/navbar.dart';
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SingleChildScrollView(
-          child: CreateEvent(),
+          child: LoadingScreen(),
         ),
       ),
     );
