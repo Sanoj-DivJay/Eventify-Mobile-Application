7a4c584e900a0b492e3d454879994a5a92d86995
Chirath Kavindu <95146759+Chira2000@users.noreply.github.com>
2023-09-20 19:40:02 +0530

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 import 'package:myapp/page-1/loading_screen.dart';
-=======
-import 'package:myapp/page-1/register.dart';
->>>>>>> Stashed changes
 import 'package:myapp/utils.dart';
 
 import 'firebase_options.dart';
@@ -35,22 +31,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: MyCustomScrollBehavior(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-<<<<<<< Updated upstream
-      home: const Scaffold(
-        body: SingleChildScrollView(
-          child: LoadingScreen(),
+        title: 'Flutter',
+        debugShowCheckedModeBanner: false,
+        scrollBehavior: MyCustomScrollBehavior(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-=======
-      home: Scaffold(
-        body: SingleChildScrollView(child: RegisterPage()),
->>>>>>> Stashed changes
-      ),
-    );
+        home: const Scaffold(
+          body: SingleChildScrollView(
+            child: LoadingScreen(),
+          ),
+        ));
   }
 }
