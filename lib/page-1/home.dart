457d46130ce277e8e418ac82327beababea759c0
Naveen Rajitha <98750933+NaveenR97@users.noreply.github.com>
2023-09-21 16:30:15 +0530

    Update home.dart

--- lib/page-1/home.dart
+++ lib/page-1/home.dart
@@ -1,1 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
 
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 393;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return Container(
+      width: double.infinity,
+      child: Container(
+        // homeSPb (128:1150)
+        padding: EdgeInsets.fromLTRB(11*fem, 14*fem, 0*fem, 12*fem),
+        width: double.infinity,
+        decoration: BoxDecoration (
+          color: Color(0xffffffff),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              // autogroupyglm937 (MExzpQKXMTrVR9NnvQYGLM)
+              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 13*fem),
+              width: 675*fem,
+              height: 368*fem,
+              child: Stack(
+                children: [
+                  Positioned(
+                    // systemlightstatusbardefaultrTK (205:1330)
+                    left: 11.5*fem,
+                    top: 0*fem,
+                    child: Container(
+                      width: 356.16*fem,
+                      height: 19*fem,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            // timeMQ5 (205:1331)
+                            margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 262.5*fem, 0*fem),
+                            child: Text(
+                              '9:41',
+                              textAlign: TextAlign.center,
+                              style: SafeGoogleFont (
+                                'SF Pro Text',
+                                fontSize: 15*ffem,
+                                fontWeight: FontWeight.w600,
+                                height: 1.2575*ffem/fem,
+                                letterSpacing: -0.3000000119*fem,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                          ),
